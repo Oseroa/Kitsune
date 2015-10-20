6dcb79e13fc7ed83e061d0fd21e9c6eac56e6f85
Aythan <aythan.maconachie.rnd@gmail.com>
2015-10-20 13:54:05 +1100

    initial commit

---
+++ Project Kitsune v0.1.b/Assets/Scripts/Blake/PlayerGlide.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGlide : MonoBehaviour
+{
+
+    public float GlidePower;
+    Rigidbody ThisRigidBody;
+    Vector3 GlideForce;
+
+    void Start()
+    {
+         GlideForce = new Vector3(0.0f, GlidePower, 0.0f);
+    }
+	void LateUpdate ()
+    {
+        ThisRigidBody = GetComponent<Rigidbody>();
+
+        ThisRigidBody.AddForce((GlideForce), ForceMode.Acceleration);
+
+	}
+}
---
+++ Project Kitsune v0.1.b/Assets/Scripts/Blake/SpiritSplit.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiritSplit : MonoBehaviour
+{
+    public GameObject SplitSpirit;
+
+    GameObject ClonedSpirit;
+
+    float CoolDownTime = 2;
+    float l_Time;
+    bool EventFlipper = false;
+    bool ActiveClone = false;
+
+    void FixedUpdate()
+    {
+        l_Time += Time.deltaTime;
+      
+
+        if (Input.GetButton("SplitSpirit") && l_Time > CoolDownTime)
+        {
+            
+           if (EventFlipper == true && ActiveClone == true)
+            {
+               
+                transform.position = ClonedSpirit.transform.position;
+                Destroy(ClonedSpirit);
+                EventFlipper = false;
+                l_Time = 0.0f;
+                ActiveClone = false;
+
+          
+            }
+
+            else if (EventFlipper == true)
+            {
+         
+                Destroy(ClonedSpirit);
+                ClonedSpirit = Instantiate(SplitSpirit, transform.position, new Quaternion(0, 0, 0, 0)) as GameObject;
+                l_Time = 0.0f;
+                ActiveClone = true;
+                EventFlipper = false;
+            }
+            EventFlipper = true;
+
+        }
+
+    }
+}
