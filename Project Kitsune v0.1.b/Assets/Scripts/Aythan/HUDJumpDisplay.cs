d0a4b0cc48ff0c881a99bdf9333daae94a611689
Aythan <aythan.maconachie.rnd@gmail.com>
2015-10-20 14:21:02 +1100

    HUD Stuff

---
+++ Project Kitsune v0.1.b/Assets/Scripts/Aythan/HUDJumpDisplay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDJumpDisplay : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] jumpObjects = new GameObject[3];
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (Input.GetButton("Jump"))
+        //{
+            for (int i = 0; i < player.GetComponent<PlayerManager>().MaxNumberOfJumps; i++)
+            {
+            if (player.GetComponent<PlayerManager>().NumberOfJumps <= i)
+            {
+                jumpObjects[i].SetActive(false);
+            }
+            else jumpObjects[i].SetActive(true);
+            }
+       // }
+       // if (player.GetComponent<PlayerManager>().IsGrounded)
+       // {
+       //     for (int i = 0; i < player.GetComponent<PlayerManager>().MaxNumberOfJumps; i++)
+       //     {
+       //         jumpObjects[i].SetActive(true);
+       //     }
+       // }
+	}
+}
