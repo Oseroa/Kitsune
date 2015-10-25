aea5c8390b3dc59a9108ba345e3d031795869103
Aythan <aythan.maconachie.rnd@gmail.com>
2015-10-26 10:35:12 +1100

    windbox trigger

---
+++ Project Kitsune v0.1.b/Assets/Scripts/Aythan/WindCollider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindCollider : MonoBehaviour
+{
+    public Vector3 windForce;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Vector3 addForce = windForce;
+        addForce.z = 0;
+        Rigidbody otherRigid = other.GetComponent<Rigidbody>();
+        if (otherRigid.mass >= 1) addForce *= 350;
+        otherRigid.AddForce(addForce);
+    }
+}
