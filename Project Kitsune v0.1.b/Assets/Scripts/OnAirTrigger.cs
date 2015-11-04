9b13c5ceec379312a0cf0d3ece3800a680160c1b
Drew Curran <vorksie@hotmail.com>
2015-11-04 13:07:48 +1100

    added lots of prefabs, player model too

---
+++ Project Kitsune v0.1.b/Assets/Scripts/OnAirTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnAirTrigger : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<PlayerManager>().NumberOfJumps = col.gameObject.GetComponent<PlayerManager>().MaxNumberOfJumps;
+
+        }
+
+    }
+
+
+}
