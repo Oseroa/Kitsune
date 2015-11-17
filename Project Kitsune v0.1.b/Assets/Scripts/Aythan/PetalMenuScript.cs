a2b9fc94c4a91e32c06fea2464568a1c083a43d0
Aythan <aythan.maconachie.rnd@gmail.com>
2015-11-17 16:32:02 +1100

    prettyful petals on prettyful menu

---
+++ Project Kitsune v0.1.b/Assets/Scripts/Aythan/PetalMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PetalMenuScript : MonoBehaviour {
+
+    public float speed = 1;
+
+    // Use this for initialization
+    void Start ()
+    {
+        transform.position = new Vector3(Random.Range(1920, 1970), Random.Range(0, 1080), 0);
+        speed = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.position -= new Vector3(1 * speed, 0, 0);
+        if (transform.position.x <= -3)
+        {
+            transform.position = new Vector3(Random.Range(1920, 1970), Random.Range(0, 1080), 0);
+            speed = Random.Range(1, 5);
+        }
+    }
+}
